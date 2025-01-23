0ec4e5d481d4bbc668f79e816e798bf41bfad656
Raphael Jay Escosia <escosiaraphael70@gmail.com>
2025-01-23 14:25:42 +0800

    first name, lastname, age, number, address

---
+++ index.js
@@ -0,0 +1,43 @@
+/*
+1. In the s03 folder, create an activity folder, an index.html file inside of it and link the index.js file.
+2. Create an index.js file and console log the message "Hello World" to ensure that the script file is properly associated with the html file.
+3. Copy the activity code from your Instructor. Paste the activity code to your index.js file.
+ */
+
+// console.log("Hello World");
+
+
+/*
+4. Create the following variables to store the following user details:
+
+    - Variable Name:
+        - firstName
+        - lastName
+        - age
+        - mobileNumber
+        - address
+*/
+
+
+
+/*
+5. Identify and implement the best practices of creating and using variables by avoiding errors and debugging the following codes. 
+    - Log the values of each variable to follow/mimic the output.
+*/
+
+let firstName = "Raphael";
+console.log("First Name:", firstName);
+
+let lastName = "Escosia";
+console.log("Last Name:", lastName);
+
+let age = 21;
+console.log("Age:", age);
+
+let mobileNumber = "09947909670";
+console.log("Mobile Number:", mobileNumber);
+
+let address = "Talisay City";
+console.log("Address:", address);
+
+
